5ccdfa96544a97a7163400ff9c6b409d28b5f6b0
Derrie N <ewenaiwe@gmail.com>
2019-09-25 01:09:40 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,6 +15,17 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
+    page = Nokogiri::HTML(open(profile_url))
+    links = page.css("social-icon-container")
+    binding.pry
+    hash = {}
+    
+    {twitter: "http://twitter.com/flatironschool",
+    linkedin: "https://www.linkedin.com/in/flatironschool",
+    github: "https://github.com/learn-co",
+    blog: "http://flatironschool.com",
+    profile_quote: "\"Forget safety. Live where you fear to live. Destroy your reputation. Be notorious.\" - Rumi",
+    bio:  "I'm a school" }
     
   end
 
