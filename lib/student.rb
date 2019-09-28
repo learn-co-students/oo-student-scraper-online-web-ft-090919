2a3b43a85623d16966c55574bfa653019c483461
Richard Pascarelli <Rpavesi84@gmail.com>
2019-09-27 21:51:56 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -10,23 +10,21 @@ class Student
     @name = student_hash[:name]
     @location = student_hash[:location]
     @@all << self
-
-
   end
 
-  def self.create_from_collection(students_array)
-    students_array.cycle(1){|stu| Student.new(stu)}
-  end
+    def self.create_from_collection(students_array)
+      students_array.cycle(1){|stu| Student.new(stu)}
+    end
 
-  def add_student_attributes(attributes_hash)
-    attributes_hash.each do |key, value|
+    def add_student_attributes(attributes_hash)
+      attributes_hash.each do |key, value|
       self.send("#{key}=",value)
-      binding.pry
+      end
+    end
+
+    def self.all
+      @@all
     end
 
 
-  def self.all
-    @@all
-  end
-end
 end
